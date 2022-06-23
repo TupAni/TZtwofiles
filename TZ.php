e1fe856eb7d003559bd2a83b70f4243a5e4216c0
TupAni <you@example.com>
2022-06-23 16:05:03 +0300

    First commit

---
+++ TZ.php
@@ -0,0 +1,17 @@
+<?php
+$a = array(1, 2, array("a", "b", "c"));
+var_dump($a);
+?>
+<form action="http://foo.com" method="get">
+    <div>
+        <label for="say">What greeting do you want to say?</label>
+        <input name="say" id="say" value="Hi">
+    </div>
+    <div>
+        <label for="to">Who do you want to say it to?</label>
+        <input name="to" id="to" value="Mom">
+    </div>
+    <div>
+        <button>Send my greetings</button>
+    </div>
+</form>
